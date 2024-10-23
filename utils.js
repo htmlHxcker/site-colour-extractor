f3fe4dfe8b8d728b0bda423312f2c475fbf8a835
Clinton <64586121+htmlHxcker@users.noreply.github.com>
2024-10-23 20:27:48 +0100

    [FEAT]: Create rgbToHex function

--- utils.js
+++ utils.js
@@ -4,3 +4,16 @@ function isLightColor(color) {
 	const luminance = (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]) / 255;
 	return luminance > 0.5;
 }
+
+function rgbToHex(rgb) {
+	const values = rgb.match(/\d+/g);
+	if (!values) return rgb;
+
+	const r = parseInt(values[0]);
+	const g = parseInt(values[1]);
+	const b = parseInt(values[2]);
+
+	return `#${r.toString(16).padStart(2, '0')}${g
+		.toString(16)
+		.padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+}
