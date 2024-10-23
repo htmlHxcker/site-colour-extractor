6640c0b1ab94c8aee6d7ff1e38ba309b1a1ed7cf
Clinton <64586121+htmlHxcker@users.noreply.github.com>
2024-10-23 20:25:09 +0100

    [FIX]: Import utils.js in HTML files

--- popup.js
+++ popup.js
@@ -87,13 +87,6 @@ function displayColors(results) {
 	});
 }
 
-function isLightColor(color) {
-	const rgb = color.match(/\d+/g);
-	if (!rgb) return false;
-	// Calculate relative luminance
-	const luminance = (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]) / 255;
-	return luminance > 0.5;
-}
 document.getElementById('showDetailedReport').addEventListener('click', () => {
 	chrome.tabs.create({ url: 'report.html' });
 });
