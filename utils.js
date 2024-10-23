166195ed30438dcaba64597a20197158763e8c35
Clinton <64586121+htmlHxcker@users.noreply.github.com>
2024-10-23 20:28:13 +0100

    [FEAT]: Create rgbToHsl function

--- utils.js
+++ utils.js
@@ -17,3 +17,42 @@ function rgbToHex(rgb) {
 		.toString(16)
 		.padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
 }
+
+function rgbToHsl(rgb) {
+	const values = rgb.match(/\d+/g);
+	if (!values) return rgb;
+
+	let r = parseInt(values[0]) / 255;
+	let g = parseInt(values[1]) / 255;
+	let b = parseInt(values[2]) / 255;
+
+	const max = Math.max(r, g, b);
+	const min = Math.min(r, g, b);
+	let h,
+		s,
+		l = (max + min) / 2;
+
+	if (max === min) {
+		h = s = 0;
+	} else {
+		const d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+		switch (max) {
+			case r:
+				h = (g - b) / d + (g < b ? 6 : 0);
+				break;
+			case g:
+				h = (b - r) / d + 2;
+				break;
+			case b:
+				h = (r - g) / d + 4;
+				break;
+		}
+		h /= 6;
+	}
+
+	return `hsl(${Math.round(h * 360)}, ${Math.round(s * 100)}%, ${Math.round(
+		l * 100
+	)}%)`;
+}
